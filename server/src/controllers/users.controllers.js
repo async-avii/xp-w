8a60afc7523017f66cacbd4fe9fd0d4fd380de52
Abhinav <abhinav.contact22@gmail.com>
2024-11-18 23:37:13 +0530

    created signup user route

--- server/src/controllers/users.controllers.js
+++ server/src/controllers/users.controllers.js
@@ -1,4 +1,26 @@
 import { prisma } from "./../app.js";
+import ApiResponse from "../utils/ApiResponse.js";
+
+export const signInUser = async (req, res) => {
+  const { fullName, email, password } = req.body;
+  try {
+    const user = await prisma.user.create({
+      data: {
+        fullName,
+        email,
+        password,
+      },
+    });
+    const response = new ApiResponse("sucess", 200, user.id);
+    return res.json(response);
+  } catch (error) {
+    console.error(error.message);
+    res.json({
+      status: 500,
+      message: "Internal error occured",
+    });
+  }
+};
 
 export const loginUser = async (req, res) => {
   const { email, password } = req.body;
--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { loginUser } from "../controllers/users.controllers.js";
+import { signInUser } from "../controllers/users.controllers.js";
 
 const router = Router();
 
+router.route("/signUp").post(signInUser);
 router.route("/login").post(loginUser);
 
 export default router;

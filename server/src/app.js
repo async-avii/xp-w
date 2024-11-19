1929b7c8a9b7db8042366e3e8b9fe5b29f9fe56e
Abhinav <abhinav.contact22@gmail.com>
2024-11-19 22:41:57 +0530

    Cookie init and implementation

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,26 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import cookieParser from "cookie-parser";
 const app = express();
 const prisma = new PrismaClient();
 
 app.use(express.json());
+app.use(cookieParser());
 
 //routes import
 import userRouter from "./routes/user.router.js";
+import organisationRouter from "./routes/organisation.router.js";
 
 app.use("/user", userRouter);
+app.use("/organisations", organisationRouter);
 
 app.get("/", (req, res) => {
+  res.cookie("token", "test");
   res.send("Hello World!");
+});
+app.get("/get-cookie", (req, res) => {
+  console.log(req.cookies);
+  res.send(req.cookies);
 });
 
 export { prisma };

a248ab272aa189f95316e148edbf1a8d72accd1b
Abhinav <abhinav.contact22@gmail.com>
2024-12-09 23:07:31 +0530

    Verify user patch

--- server/src/routes/user.router.js
+++ server/src/routes/user.router.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
-import { loginUser } from "../controllers/users.controllers.js";
+import {
+  getUser,
+  loginUser,
+  verifyUser,
+} from "../controllers/users.controllers.js";
 import { signUpUser } from "../controllers/users.controllers.js";
 
 const router = Router();
 
 router.route("/signUp").post(signUpUser);
 router.route("/login").post(loginUser);
+router.route("/:id").get(getUser);
+router.route("/:id").patch(verifyUser);
 
 export default router;
